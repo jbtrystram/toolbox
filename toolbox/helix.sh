b27df73c4f5d0c185cf31d36b74fa9ea4798b946
jbtrystram <jbtrystram@redhat.com>
2024-10-10 16:10:49 +0200

    add markdown lsp for helix

--- toolbox/helix.sh
+++ toolbox/helix.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 # Reference : 
 # https://github.com/helix-editor/helix/wiki/Language-Server-Configurations#toml
 
@@ -12,3 +14,7 @@ go install golang.org/x/tools/cmd/goimports@latest
 
 pip install -U 'python-lsp-server[all]'
 
+# Marksman for markdown support
+curl -L https://github.com/artempyanykh/marksman/releases/latest/download/marksman-linux-x64 \
+ -o /usr/local/bin/marksman
+chmod +x /usr/local/bin/marksman
