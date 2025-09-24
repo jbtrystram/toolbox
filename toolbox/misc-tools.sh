b475228d64fc4e80fd906068dd75a58bf5ad9293
jbtrystram <jbtrystram@redhat.com>
2025-09-24 17:28:04 +0200

    pull tkn from ocp repos

--- toolbox/misc-tools.sh
+++ toolbox/misc-tools.sh
@@ -7,7 +7,6 @@ set -euxo pipefail
 npm install -g @google/gemini-cli
 
 # tekton cli
-dnf copr enable chmouel/tektoncd-cli
-dnf install tektoncd-cli
-dnf copr remove chmouel/tektoncd-cli
-
+curl -L https://mirror.openshift.com/pub/openshift-v4/clients/pipelines/latest/tkn-linux-amd64.tar.gz -o tkn.tar.gz
+tar xvf tkn.tar.gz -C /usr/local/bin/ --no-same-owner
+rm -f tkn.tar.gz
