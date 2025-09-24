9bedd2e93c817b76499950f8500c1117529c5417
jbtrystram <jbtrystram@redhat.com>
2025-09-24 14:22:08 +0200

    add tkn cli

--- toolbox/misc-tools.sh
+++ toolbox/misc-tools.sh
@@ -5,3 +5,9 @@ set -euxo pipefail
 # Gemini CLI
 
 npm install -g @google/gemini-cli
+
+# tekton cli
+dnf copr enable chmouel/tektoncd-cli
+dnf install tektoncd-cli
+dnf copr remove chmouel/tektoncd-cli
+
