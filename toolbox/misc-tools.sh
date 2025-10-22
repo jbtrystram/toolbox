2f0491d193f1e0185e474f6bab19fd338fbbf7ff
jbtrystram <jbtrystram@redhat.com>
2025-10-22 12:38:35 +0200

    tools: add act

--- toolbox/misc-tools.sh
+++ toolbox/misc-tools.sh
@@ -10,3 +10,9 @@ npm install -g @google/gemini-cli
 curl -L https://mirror.openshift.com/pub/openshift-v4/clients/pipelines/latest/tkn-linux-amd64.tar.gz -o tkn.tar.gz
 tar xvf tkn.tar.gz -C /usr/local/bin/ --no-same-owner
 rm -f tkn.tar.gz
+
+# To test GH actions
+# https://github.com/nektos/act/
+curl -LO https://github.com/nektos/act/releases/latest/download/act_Linux_x86_64.tar.gz
+tar xvf act_Linux_x86_64.tar.gz -C /usr/local/bin act
+rm -f act_Linux_x86_64.tar.gz
