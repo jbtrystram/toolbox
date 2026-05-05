655a448d49c19d25f9a20bd1908ae190be3cb016
jbtrystram <jbtrystram@redhat.com>
2026-05-05 10:33:06 +0200

    drop gemini CLI

--- misc-tools.sh
+++ misc-tools.sh
@@ -2,10 +2,6 @@
 
 set -euxo pipefail
 
-# Gemini CLI
-
-npm install -g @google/gemini-cli
-
 # tekton cli
 curl -L https://mirror.openshift.com/pub/openshift-v4/clients/pipelines/latest/tkn-linux-amd64.tar.gz -o tkn.tar.gz
 tar xvf tkn.tar.gz -C /usr/local/bin/ --no-same-owner
